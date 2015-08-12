e10ac2a65ce5aed75c495ec1f30377147f80348d
envido32 <envido32@gmail.com>
2015-08-12 01:06:22 -0300

    implements SharedPreferences.OnSharedPreferenceChangeListener

--- app/src/main/java/ar/edu/frba/utn/gibio/fisioremoto/NewPersonActivity.java
+++ app/src/main/java/ar/edu/frba/utn/gibio/fisioremoto/NewPersonActivity.java
@@ -3,6 +3,8 @@ package ar.edu.frba.utn.gibio.fisioremoto;
 
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.EditTextPreference;
+import android.preference.Preference;
 import android.preference.PreferenceFragment;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
@@ -29,14 +31,49 @@ public class NewPersonActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
-    public static class NewPersonPreferenceFragment extends PreferenceFragment
+    public static class NewPersonPreferenceFragment extends PreferenceFragment implements SharedPreferences.OnSharedPreferenceChangeListener
     {
+        private static final String KEY_EDIT_TEXT_PREFERENCE = "name";
+
         @Override
         public void onCreate(final Bundle savedInstanceState)
         {
             super.onCreate(savedInstanceState);
             addPreferencesFromResource(R.xml.new_person);
             addPreferencesFromResource(R.xml.new_medical);
+        }
+
+        @Override
+        public void onResume(){
+            super.onResume();
+            getPreferenceScreen().getSharedPreferences().
+                    registerOnSharedPreferenceChangeListener(this);
+            updatePreference(KEY_EDIT_TEXT_PREFERENCE);
+        }
+
+        @Override
+        public void onPause() {
+            super.onPause();
+            getPreferenceScreen().getSharedPreferences().
+                    unregisterOnSharedPreferenceChangeListener(this);
+        }
+        @Override
+        public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+            updatePreference(key);
+        }
+
+        private void updatePreference(String key){
+            if (key.equals(KEY_EDIT_TEXT_PREFERENCE)){
+                Preference preference = findPreference(key);
+                if (preference instanceof EditTextPreference){
+                    EditTextPreference editTextPreference =  (EditTextPreference)preference;
+                    if (editTextPreference.getText().trim().length() > 0){
+                        editTextPreference.setSummary("Entered Name is  " + editTextPreference.getText());
+                    }else{
+                        editTextPreference.setSummary("Enter Your Name");
+                    }
+                }
+            }
         }
     }
 
